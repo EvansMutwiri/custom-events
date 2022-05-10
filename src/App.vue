aa7c3fd0d7a8772cf1fdbc341375a12aa6a12a88
EvansMutwiri <mutwirievansm@gmail.com>
2022-05-10 08:50:29 +0300

    component events emit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import PopUp from './components/PopUp.vue';
+
+// export default {
+  // components: {
+  //   PopUp
+  // },
+  // data() {
+  //   return {
+  //     isMenuOpen: false
+  //   }
+  // }
+  
+// }
+
+//using setup method
+
+const isMenuOpen = ref(false);
+
+</script>
+
+<template>
+
+  <body class="bg-lime-400 h-screen">
+    <header class="h-1/5">
+
+    </header>
+
+    <div class="flex justify-center">
+      <button class="bg-sky-200 p-4" @click="isMenuOpen = true">Show Alert</button>
+    </div>
+
+    <!-- v-if instead of v-show -->
+    <pop-up v-if="isMenuOpen" @closePopUp="isMenuOpen = false"/>
+
+  </body>
+
+</template>
+
+<style>
+@import './assets/tailwind.css';
+</style>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-white px-6 py-4 border-0 rounded relative mt-4 w-1/7 h-1/6 bg-lime-200">
+      <span class="text-xl inline-block mr-5 align-middle">
+        <i class="fas fa-bell" />
+      </span>
+      <span class="inline-block align-middle mr-8">
+        <b class="capitalize">Pop up</b>!
+      </span>
+    </div>
+
+    <div class="flex justify-center mt-4">
+      <button class="bg-sky-200 mx-auto p-4" @click="$emit('closePopUp')">Close</button>
+    </div>
+</template>
+
+<script setup>
+  const emit = defineEmits('closePopUp')
+</script>
+
+<style>
+
+</style>
